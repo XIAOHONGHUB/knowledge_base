1d739ff65da01e4867e2a52f64ca0c0a0bde6e27
15185626476 <2446434034@qq.com>
2024-09-09 16:50:00 +0800

    软考笔记提交

--- docs/.vuepress/config/nav.js
+++ docs/.vuepress/config/nav.js
@@ -14,7 +14,7 @@ module.exports = [
     text: '技术',
     link: '/examination/',
     items: [
-      {text: '技术文档', link: '/pages/9a7ee40fc232253e/'},
+      {text: '技术', link: '/pages/9a7ee40fc232253e/'},
       {text: '博客搭建', link: '/pages/41f87d890d0a02af/'},
     ]
   },
