ff054914981a0326a0e4e6cc17c733add1774ee7
15185626476 <2446434034@qq.com>
2024-10-11 16:01:31 +0800

    tdas

--- docs/.vuepress/config/nav.js
+++ docs/.vuepress/config/nav.js
@@ -18,5 +18,14 @@ module.exports = [
       {text: '博客搭建', link: '/pages/41f87d890d0a02af/'},
     ]
   },
-  {text: '归档', link: '/archives/'}
+  {text: '归档', link: '/archives/'},
+  {
+    text: '索引',
+    link: '/archives/',
+    items: [
+      { text: '分类', link: '/categories/' },
+      { text: '标签', link: '/tags/' },
+      { text: '归档', link: '/archives/' },
+    ],
+  },
 ]
